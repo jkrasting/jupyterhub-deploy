94b2d0c7a12e87edca60a381bc849507c3f642f8
John Krasting <John.Krasting@noaa.gov>
2025-11-04 09:31:35 -0500

    mapped username

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,4 +1,4 @@
-# /opt/jupyterhub/jupyterhub_config.py (v4.7 - The Complete Official Method)
+# /opt/jupyterhub/jupyterhub_config.py (v5.0 - Fixed UID/GID Mapping)
 import os
 import pwd
 import grp
@@ -6,24 +6,35 @@ import grp
 # --- Pre-Spawn Hook for UID/GID Mapping ---
 def pre_spawn_hook(spawner):
     username = spawner.user.name
+    spawner.log.info(f"Pre-spawn hook called for user: {username}")
+    
     try:
+        # Look up the user on the host system
         user_info = pwd.getpwnam(username)
         uid = user_info.pw_uid
         gid = user_info.pw_gid
-        # Set the environment variables the jupyter/docker-stacks images expect
+        
+        spawner.log.info(f"Found user '{username}' with UID={uid}, GID={gid}")
+        
+        # Set the environment variables that jupyter/docker-stacks images use
         spawner.environment['NB_UID'] = str(uid)
         spawner.environment['NB_GID'] = str(gid)
-        spawner.environment['GRANT_SUDO'] = 'yes'
+        spawner.environment['NB_USER'] = username
+        spawner.environment['CHOWN_HOME'] = 'yes'
+        spawner.environment['CHOWN_HOME_OPTS'] = '-R'
+        
     except KeyError:
-        spawner.log.warning(
-            f"User '{username}' not found on the host. "
-            "Spawning container with default UID/GID."
+        spawner.log.error(
+            f"User '{username}' not found in /etc/passwd on the host. "
+            "Container will spawn with default UID/GID (1000)."
         )
+        raise
 
-# --- Authenticator Configuration (no changes) ---
+c.DockerSpawner.pre_spawn_hook = pre_spawn_hook
+
+# --- Authenticator Configuration ---
 from oauthenticator.generic import GenericOAuthenticator
 c.JupyterHub.authenticator_class = GenericOAuthenticator
-# ... (all your GenericOAuthenticator settings remain the same) ...
 c.GenericOAuthenticator.client_id = os.environ.get('OAUTH_CLIENT_ID')
 c.GenericOAuthenticator.client_secret = os.environ.get('OAUTH_CLIENT_SECRET')
 c.GenericOAuthenticator.oauth_callback_url = 'https://jupyterhub.krasting.org/hub/oauth_callback'
@@ -33,7 +44,7 @@ c.GenericOAuthenticator.token_url = os.environ.get('OAUTH_TOKEN_URL')
 c.GenericOAuthenticator.userdata_url = os.environ.get('OAUTH_USERDATA_URL')
 c.GenericOAuthenticator.username_claim = "preferred_username"
 
-# --- Authorization Configuration (no changes) ---
+# --- Authorization Configuration ---
 c.Authenticator.allowed_users = {'krasting', 'another_user'}
 c.Authenticator.admin_users = {'krasting'}
 
@@ -43,17 +54,20 @@ c.JupyterHub.spawner_class = DockerSpawner
 c.DockerSpawner.image = 'jupyter/scipy-notebook:latest'
 c.DockerSpawner.network_name = 'jupyterhub-network'
 c.DockerSpawner.remove = True
-c.DockerSpawner.volumes = {
-    '/home/{username}': '/home/jovyan/work'
-}
-c.DockerSpawner.pre_spawn_hook = pre_spawn_hook
 
-# THIS IS THE FIX:
-# Re-add this line. The container MUST start as root for the
-# NB_UID/NB_GID environment variables to be effective.
+# Volume mapping - mount host user's home to /home/jovyan (not /home/jovyan/work)
+c.DockerSpawner.volumes = {
+    '/home/{username}': '/home/jovyan'
+}
+
+# CRITICAL: Container must start as root for NB_UID/NB_GID to work
+# The jupyter/docker-stacks start.sh script will switch to the correct user
 c.DockerSpawner.user = 'root'
 
-# --- Network & URL Configuration (no changes) ---
+# Format the volume paths with the actual username
+c.DockerSpawner.format_volume_name = lambda name, spawner: name.format(username=spawner.user.name)
+
+# --- Network & URL Configuration ---
 c.JupyterHub.hub_ip = '0.0.0.0'
 c.JupyterHub.hub_connect_ip = 'jupyterhub'
 c.JupyterHub.default_url = '/hub/home'
